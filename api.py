0be39dc1cef879269d02308ce66d757410035857
rehaqds <rehahnq@gmail.com>
2022-08-11 10:52:57 +0200

    11/08/2022 latest updates

--- api.py
+++ api.py
@@ -1,67 +1,92 @@
+# Define the API
+#    Take a json (customer id) in and a json out (class and proba default)
+#    Deployed on Heroku
+#    Used by Streamlit app
+#    Actually don't need this API as work could be done in Streamlit script
+#       directly (but useful for practice/learn)
+
+# To test locally: in one CLI: python api.py (& http://localhost:5000)
+#    in a 2nd CLI: launch streamlit in local: streamlit run app_streamlit.py
+
+
+
+import joblib
+# import requests
+# import json
+import numpy as np
+# import sklearn
+# import xgboost
+import catboost   # used by joblib
 from flask import Flask, request, jsonify
-import json 
-import pickle, joblib
-#import requests
-#import pandas as pd
-import sklearn
-import xgboost
+
+
+
+THRESHOLD1 = 0.065
 
 
 # Your API definition
 app = Flask(__name__)
     # name= '__main__' if run directly. if imported: name of file
 app.config["DEBUG"] = True
-    # lance le débogueur, ce qui permet d’afficher un message autre que 
+    # lance le débogueur, ce qui permet d’afficher un message autre que
     #    « Bad Gateway » s’il y a une erreur dans l’application
 
 
+# Load the data
+train_df = joblib.load('data/train_df.jlb')
+model = joblib.load('data/p7-model.jlb')
+test_df = joblib.load('data/test_df.jlb')
 # with open('p7-model.pkl', 'rb') as f1:
 #     model = pickle.load(f1)
-with open('train_df.pkl', 'rb') as f2:
-    train_df = pickle.load(f2)  
-model = joblib.load('p7-model.jlb')  
-test_df = joblib.load('test_df.jlb')
-
-print ('Model loaded\n')
+print('Model loaded\n')
 
 
 
-# the decorator associates a function to a url
+# The decorator associates a function to a url
 # flask envoie des requêtes http à des fonctions (routage)
+# Main page
 @app.route('/')
 def start():
+    """."""
     return('<h1>P7 API running</h1>')
     # will be displayed on the root page: http://localhost:5000/
     #   or https://bank-app-oc.herokuapp.com/ if deployed to heroku
 
 
+
+# Predict page
 @app.route('/predict', methods=['POST'])
 def predict():
-    json_ = request.json
-    # json_ = json.loads(json_.decode("utf-8"))
+    """."""
+    json_ = request.json  # json received as input (customer ID)
+    # json_ = json.loads(json_.de%tbcode("utf-8"))
     print(json_)
-    #cust = test_df[test_df['SK_ID_CURR'] == json_['id']]
+    # cust = test_df[test_df['SK_ID_CURR'] == json_['id']]
     cust = test_df.loc[[json_['id']]]
-    #cust = train_df.loc[[json_['id']]]
-    # cust = train_df.iloc[[0]]   
-        # need a list to get a df and not a series size (53,)
+        # need a list to get a df size (53,1) and not a series size (53,)
 
-    prediction = model.predict(cust.values)  #don't forget .values !!!
-    print(prediction)
+    # As .predict use 0.5 threshold, needs to use predict_proba
+    # prediction = model.predict(cust.values)  # don't forget .values !!!
+    # [0, 1]: O for the first and only line in the array and 1 to get the
+    # probability for class 1
+    proba = model.predict_proba(cust.values)[0, 1]
+    prediction = np.where(proba < THRESHOLD1, 0, 1)
+    print(prediction, proba)
 
-    return jsonify({'score': str(prediction)})  #to json format
+    return jsonify({'class': str(prediction),
+                    'proba': str(proba),
+                    'seuil': str(THRESHOLD1)
+            })  # to json format (must be a string to be json serializable)
 
 
 
-
-# lance le serveur (remove debug when not needed) 
+# lance le serveur (remove debug when not needed)
 # adresse IP du serveur local: http://127.0.0.1:5000 (=http://localhost:5000)
 if __name__ == '__main__':
 
 
-    app.run() # en phase production
-    #app.run(debug=True, use_reloader=False)  # en phase prog #port=9010
+    app.run()  # en phase production
+
+    # app.run(debug=True, use_reloader=False)  # en phase prog #port=9010
     # use_reloader=False needed when debug=True if run from spyder/ipython
     #   if not run api.py from the CLI
-    
-    
--- app_streamlit.py
+++ app_streamlit.py
@@ -1,70 +1,283 @@
+# Streamlit app
+#    To run, in a CLI:   streamlit run app_streamlit.py
+
+
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle, joblib
+import joblib
 import requests
+from PIL import Image
+import shap
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-test_df = joblib.load('test_df.jlb')
 
-app_mode = st.sidebar.selectbox('Select Page',['Home','Predict_Churn'])
+CLOUD = True  # True: deployment on the cloud / False: local
 
-# Page Home
-if app_mode=='Home': 
-    st.title('Employee Prediction') 
-    st.markdown('Dataset :') 
-    #df=pd.read_csv('emp_analytics.csv') #Read our data dataset
-    st.write(test_df.head()) 
-    
+st.set_option('deprecation.showPyplotGlobalUse', False)
+# mpl.rcParams["font.size"] = 3
 
 
-# Page predict    
-elif app_mode == 'Predict_Churn':
-## specify our inputs
-    st.subheader('Fill in employee details to get prediction ')
-    st.sidebar.header("Other details :")
-    prop = {'salary_low': 1, 'salary_high': 2, 'salary_medium': 3}
-    satisfaction_level = st.number_input("satisfaction_level", min_value=0.0, 
-                                         max_value=1.0)
-    average_montly_hours = st.number_input("average_montly_hours")
-    promotion_last_5year = st.number_input("promotion_last_5year")
-    salary = st.sidebar.radio("Select Salary ",tuple(prop.keys()))
 
-    salary_low,salary_medium,salary_high=0,0,0
-    if salary == 'High':
-        salary_high = 1
-    elif salary == 'Low':
-        salary_low = 1
-    else :salary_medium = 0
+@st.cache   # put in memory for faster rendering at each update
+def get_shap(model, train_df, test_df):
+    """Compute SHAP values."""
+    # # If compute shap inside the app (but slow):
+    # explainer = shap.explainers.Tree(model)
+    # shap_values = explainer.shap_values(test_df)
+    # exp_shap_values = explainer(test_df)
+    # shap_values_train = explainer.shap_values(train_df)
+
+    # if load shap from pickle
+    shap_values, exp_shap_values, shap_values_train = \
+        joblib.load('data/shap.jlb')
+    return shap_values, exp_shap_values, shap_values_train
 
 
-    subdata={
-        'satisfaction_level':satisfaction_level,
-        'average_montly_hours ':average_montly_hours ,
-        'promotion_last_5year':   promotion_last_5year,
-        'salary':[salary_low,salary_medium,salary_high],
-        }
+# @st.cache
+def plot_global_imp(shap_values_train, train_df):
+    """."""
+    g4 = shap.summary_plot(shap_values_train, train_df, plot_size=(12, 8),
+                           # show=False
+                           )
+    return g4  # , plt.gcf()
 
-    features = [satisfaction_level, average_montly_hours, promotion_last_5year, 
-                subdata['salary'][0],subdata['salary'][1], subdata['salary'][2]]
 
-    results = np.array(features).reshape(1, -1)
-    
-    
-if st.button("Predict"):
+def calc_score(x, seuil):
+    """Compute the score based on the 'proba' of class 1."""
+    if x < seuil:
+        res = 0.5 / seuil * x
+    else:
+        res = x * 0.5 / (1 - seuil) + (0.5 - seuil) / (1 - seuil)
+    return int((1 - res) * 100)
 
-    # picklefile = open("emp-model.pkl", "rb")
-    # model = pickle.load(picklefile)
 
-    # prediction = model.predict(results)
-    # if prediction[0] == 0:
-    #     st.success('Employee will not churn')
-    # elif prediction[0] == 1:
-    #     st.error( 'Employee will churn')
-        
-    res = requests.post('https://bank-app-oc.herokuapp.com//predict', 
-                        json={'id':30000}) #test
-    #if res.ok:
-    st.write(res.ok)
-    st.write(res.json())
-        
+def display_title():
+    """."""
+    st.markdown("<h1 style='text-align: center; color: blue;'>"
+                "Prêt à Dépenser</h1>", unsafe_allow_html=True)
+
+    st.markdown("<h2 style='text-align: center; color: black;'>"
+                "Credit Scoring </h2>", unsafe_allow_html=True)
+    st.write('') ; st.write('')
+
+
+def display_results(res):
+    """."""
+    # Score
+    score = calc_score(float(res['proba']), float(res['seuil']))
+    if int(res['class']) == 0:
+        st.success('LOAN APPROVED')
+        st.write('')
+        st.metric(label="Sccore: ",
+                  value=str(score) + '/100',
+                  delta="Approved")
+    elif int(res['class']) == 1:
+        st.error('LOAN REJECTED')
+        st.write('')
+        st.metric(label="Score: ",
+                  value=str(score) + '/100',
+                  delta="-Rejected")
+    st.write('(', round(float(res['proba']), 2), ')')
+    st.write('') ; st.write('')
+
+
+    # SHAP plots
+
+    st.markdown('### Influence of the top factors about the decision '
+                'for the customer (local):')
+    row_n = test_df.index.get_loc(customer_id)  # get line n°
+    # explainer = shap.TreeExplainer(model)
+    # shap_values = explainer(test_df)  # w/ cache?
+    shap_values, exp_shap_values, shap_values_train = \
+        get_shap(model, train_df, test_df)
+
+
+    # Waterfall plot
+
+    # fig = plt.figure(figsize=(6, 2))
+    # plt.figure(figsize=(0.1,0.1))
+    g1 = shap.plots.waterfall(exp_shap_values[row_n], max_display=15)
+    # fig = plt.gcf() ; fig.set_size_inches(2, 2) ; fig.set_dpi(2)
+    # _, h = plt.gcf().get_size_inches()
+    # plt.gcf().set_size_inches(h*5, h)
+    st.pyplot(g1)
+    st.write('') ; st.write('')
+
+
+    # Distribution plots
+
+    st.markdown('### Cross-comparison on 1 factor:')
+    # st.write(shap_values[row_n].values)
+    # feature_names2 = test_df.columns[
+    #     np.argsort(np.abs(shap_values[row_n].values))]
+    # feature_names = shap_values[row_n].feature_names  # ordered by shap????
+    # st.write(feature_names[:5])
+    # st.write(feature_names2[:5])
+
+    feature_names = exp_shap_values.feature_names
+    shap_df = pd.DataFrame(exp_shap_values.values, index=test_df.index,
+                           columns=feature_names)  # if not lgbm
+
+    # vals= np.round(np.abs(shap_df[row_n].values), 6)
+    vals = np.round(np.abs(shap_df.loc[customer_id].values), 6)
+    feat_imp = pd.DataFrame(list(zip(test_df.columns, vals)),
+                            columns=['col_name', 'feature_importance_vals'])
+    feat_imp.sort_values(by=['feature_importance_vals'], ascending=False,
+                         inplace=True)
+    # st.write(feat_imp[:10])
+    # st.write(feat_imp['col_name'], feat_imp.iloc[0]['col_name'])
+
+    var_plot = st.multiselect('Choose the features to plot',
+                              feat_imp['col_name'],
+                              feat_imp.iloc[0]['col_name'])
+                              # feature_names, feature_names[0])
+    full_train = pd.concat([train_df, Y_df], axis=1)
+
+    fig = None
+    for var in var_plot:
+        g2 = sns.kdeplot(data=full_train, x=var, hue='TARGET',
+                         common_norm=False)
+        val_cust = test_df.loc[customer_id, var]
+        g2.axvline(x=val_cust, color='g', ls='--', label='cust')
+        fig = plt.gcf()
+        fig.set_size_inches(2, 0.5)  # fig.set_dpi(500)
+        st.pyplot()  # fig)
+
+    st.write('')
+    st.write('Target 0 : clients with no payment difficulties')
+    st.write('Target 1 : clients with payment difficulties: late payment more'
+             'than X days on at least one of the first Y installments of the'
+             'loan in our sample')
+    st.write('') ; st.write('')
+
+
+
+    # Bi-variate plot
+
+    st.markdown('### Cross-comparison on 2 factors:')
+    var_bi = st.multiselect('Choose the 2 factors to plot',
+                            feat_imp['col_name'],
+                            [feat_imp.iloc[0]['col_name'],
+                             feat_imp.iloc[1]['col_name']],
+    )
+    # g3 = sns.jointplot(x=full_train[var_bi[0]],
+    #                    y=full_train[var_bi[1]],
+    #                    hue=full_train['TARGET'],
+    #                    marker="s", s=20, palette='Set1',  # kind="kde"
+    #                    height=8, ratio=8)
+    # g3.ax_joint.scatter(x=test_df.loc[customer_id, var_bi[0]],
+    #                     y=test_df.loc[customer_id, var_bi[1]],
+    #                     marker='o', color='g', s=200)
+    # g3.ax_joint.set_xlim(-0.01, 1.0) ; g.ax_joint.set_ylim(-0.01, 1.0)
+
+    g3 = sns.JointGrid(data=full_train, x=var_bi[0], y=var_bi[1],
+                       hue='TARGET', palette='Set1',  # kind="kde"
+                       height=8, ratio=8, xlim=(-0.01, 1.0), ylim=(-0.01, 1.0))
+    g3.plot_joint(sns.scatterplot, marker="s", s=20)
+    g3.plot_marginals(sns.kdeplot, common_norm=False)
+    g3.ax_joint.scatter(x=test_df.loc[customer_id, var_bi[0]],
+                        y=test_df.loc[customer_id, var_bi[1]],
+                        marker='o', color='g', s=200)
+
+    # fig = plt.gcf() ; fig.set_size_inches(2,0.5) #; fig.set_dpi(500)
+    st.pyplot(g3)  # , clear_figure=True)
+    st.write('') ; st.write('')
+    plt.clf()
+
+
+    # Customer Data
+
+    st.markdown('### Customer data:')
+    st.dataframe(test_df.loc[customer_id].T)
+    st.write('') ; st.write('')
+
+
+    # Best global features for the model (based on permutation importance)
+
+    st.markdown('### Influence of the top factors '
+                'on the model (global):')
+    g4 = plot_global_imp(shap_values_train, train_df)
+    # plt.gcf().axes[-1].set_aspect(100)
+    # plt.gcf().axes[-1].set_box_aspect(100)
+    # fig = plt.gcf() ; fig.set_size_inches(2,0.5) #; fig.set_dpi(500)
+    st.pyplot(g4)
+    st.write('') ; st.write('')
+
+
+
+# Load the data
+model = joblib.load('data/p7-model.jlb')
+test_df = joblib.load('data/test_df.jlb')
+train_df = joblib.load('data/train_df.jlb')
+Y_df = joblib.load('data/Y_df.jlb')
+
+
+
+# -- SIDE BAR
+
+image = Image.open('img/logo.png')
+col1, col2, col3 = st.sidebar.columns([1, 2, 5])  # to center the image
+with col2:
+    st.image(image, caption='', width=200)  # must remove sidebar inside 'with'
+_, col2b, _ = st.sidebar.columns([0.5, 1, 0.5])  # to center the selectbox
+with col2b:
+    customer_id = st.selectbox('Select the customer ID',
+                               test_df.index)
+
+# _, col2c, _ = st.sidebar.columns([1,1,1])
+# with col2c:
+#     button_score = st.button('Get Score')
+# if button_score:
+#     # FIRST_LOAD = False
+#     # display_title()
+#     # res0 = requests.post('https://bank-app-oc.herokuapp.com//predict',
+#     #                      json={'id':customer_id}) #test
+#     res0 = requests.post('http://localhost:5000/predict',
+#                          json={'id':customer_id})
+#     res = res0.json()
+#     # display_results(res)
+
+
+
+
+# -- MAIN PAGE
+
+display_title()
+
+# Send a POST request to the API to getcompute the customer score
+if CLOUD:
+    res0 = requests.post('https://bank-app-oc.herokuapp.com//predict',
+                         json={'id': customer_id})
+else:
+    res0 = requests.post('http://localhost:5000/predict',
+                         json={'id': customer_id})
+display_results(res0.json())
+
+
+
+
+# -- THE END --
+
+
+
+
+# # if app_mode=='Home':
+# # st.title('Employee Prediction')
+# st.markdown('Data :')
+# #df=pd.read_csv('emp_analytics.csv') #Read our data dataset
+# st.dataframe(test_df.head())
+
+# # Page predict
+# elif app_mode == 'Predict_Churn':
+# ## specify our inputs
+#     st.subheader('Fill in employee details to get prediction ')
+#     st.sidebar.header("Other details :")
+#     prop = {'salary_low': 1, 'salary_high': 2, 'salary_medium': 3}
+#     satisfaction_level = st.number_input("satisfaction_level", min_value=0.0,
+#                                          max_value=1.0)
+
+# for shap: st.pyplot(shap.plots.force(shaps_values[0],matplotlib=True))
